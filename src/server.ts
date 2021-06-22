bfd88acbdbb33c1792d3dbf42f73b298b9877caf
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-06-22 08:17:48 -0300

    create user

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
+import 'reflect-metadata'
 import express from 'express'
+import './database/connection'
+import { router } from './routes'
 
 const app = express()
 app.use(express.json())
-
-app.get("/", (request, response) =>  {
-  return response.json({message: "valoriza API"})
-})
-
+app.use(router)
 app.listen(3000, () => console.log("Server runnig!"))
---
+++ src/service/CreateUSerService.ts
@@ -0,0 +1,31 @@
+import { getCustomRepository } from "typeorm"
+import { UserRepository } from "../repositories/UserRepository"
+
+interface IUserRequest {
+  name: string,
+  email: string,
+  admin?: boolean
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest) {
+    const userRepository =  getCustomRepository(UserRepository) 
+
+    const userExists = await userRepository.findOne({email})
+
+    if(userExists) {
+      throw new Error("User already exists")
+    }
+
+    const user = userRepository.create({
+      name,
+      email,
+      admin
+    })
+
+    await userRepository.save(user)
+    return user
+  }
+}
+
+export { CreateUserService }
