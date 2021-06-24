e477a0c2788bd021190acd350a83461495134310
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-06-24 08:57:53 -0300

    new column password

--- src/service/CreateUSerService.ts
+++ src/service/CreateUSerService.ts
@@ -1,14 +1,16 @@
 import { getCustomRepository } from "typeorm"
 import { UserRepository } from "../repositories/UserRepository"
+import { hash } from 'bcryptjs'
 
 interface IUserRequest {
-  name: string,
-  email: string,
+  name: string
+  email: string
+  password: string
   admin?: boolean
 }
 
 class CreateUserService {
-  async execute({ name, email, admin }: IUserRequest) {
+  async execute({ name, email, admin, password }: IUserRequest) {
     const userRepository =  getCustomRepository(UserRepository) 
 
     const userExists = await userRepository.findOne({email})
@@ -17,10 +19,13 @@ class CreateUserService {
       throw new Error("User already exists")
     }
 
+    const passwordHash = await hash(password, 8)
+
     const user = userRepository.create({
       name,
       email,
-      admin
+      admin,
+      password: passwordHash
     })
 
     await userRepository.save(user)
