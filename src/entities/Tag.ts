377a5e5d2bf08c4ef8e7df8bd558cbb3a5cbc6b1
Allan Da Silva Pessin <allan.pessin@outlook.com>
2021-06-27 10:31:49 -0300

    class-transformer add to exclude password

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -1,3 +1,4 @@
+import { Expose } from "class-transformer";
 import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
 import { v1 as uuid } from 'uuid'
 
@@ -15,6 +16,10 @@ class Tag {
   @UpdateDateColumn()
   updated_at: Date
 
+  @Expose({name: "name_custom"})
+  name_custom(): string {
+    return `#${this.name}`
+  }
   constructor() {
     if (!this.id) {
       this.id = uuid()
